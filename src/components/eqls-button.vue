afa9cfc9b6b6f78dab8bf3112513ee13c4651bb6
Bhargav <bhargavbhimani55@gmail.com>
2023-10-27 11:34:20 +0530

    initial commit

---
+++ src/components/eqls-button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button
+        class="text-white bg-orange-500 w-16 sm:w-28 md:w-44 lg:w-56 h-16 text-xl font-semibold cursor-pointer rounded-full flex justify-center items-center hover:bg-orange-600">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/components/operator-button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button
+        class="text-white bg-slate-600 w-16 sm:w-28 md:w-44 lg:w-56  h-16 text-xl font-semibold cursor-pointer rounded-full flex justify-center items-center hover:bg-slate-700">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
